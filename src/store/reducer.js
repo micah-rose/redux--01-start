85c8f05f59a23dd0bacaaeafecb088e30b476bb9
Micah Lund <micah.r.lund@gmail.com>
2020-07-23 23:54:27 -0600

    Outsourced action types.

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,3 +1,5 @@
+import * as actionTypes from './actions';
+
 const initialState = {
     counter: 0,
     results: []
@@ -5,40 +7,40 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case 'INCREMENT':
+        case actionTypes.INCREMENT:
             return {
                 ...state,
                 counter: state.counter + 1
             }
-        case 'DECREMENT':
+        case actionTypes.DECREMENT:
             return {
                 ...state,
                 counter: state.counter - 1
             }
-        case 'ADD':
+        case actionTypes.ADD:
             return {
                 ...state,
                 counter: state.counter + action.value
             }
-        case 'SUBTRACT':
+        case actionTypes.SUBTRACT:
             return {
                 ...state,
                 counter: state.counter - action.value
             } 
-        case 'STORE_RESULT':
+        case actionTypes.STORE_RESULT:
             return {
                 ...state,
                 results: state.results.concat({id: new Date(), value: state.counter})
             }
-            case 'DELETE_RESULT':
-                // const id =2;
-                // const newArray = [...state.results];
-                // newArray.splice(id, 1)
-                const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-                return {
-                    ...state,
-                    results: updatedArray
-                }
+        case actionTypes.DELETE_RESULT:
+            // const id =2;
+            // const newArray = [...state.results];
+            // newArray.splice(id, 1)
+            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+            return {
+                ...state,
+                results: updatedArray
+            }
         default: return state;    
         }      
     }   
